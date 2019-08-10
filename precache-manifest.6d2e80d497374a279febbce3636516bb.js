feb703c2e346208cc1c70de2a32b2ab7f386b2e0
spbryan <seanbryan@comcast.com>
2019-08-10 09:27:36 -0400

    Updates

---
+++ precache-manifest.6d2e80d497374a279febbce3636516bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb6909e420894628d4a00b385a21c74",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3349d84ff2885b097753",
+    "url": "/clicky-game/static/css/main.19bde385.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3349d84ff2885b097753",
+    "url": "/clicky-game/static/js/main.b0d85642.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "a614cf48c86b1ed787d5b37490be6ccf",
+    "url": "/clicky-game/static/media/background6.a614cf48.jpg"
+  },
+  {
+    "revision": "1774af69ee6a7f11bbd8d2b233026176",
+    "url": "/clicky-game/static/media/header.1774af69.jpg"
+  },
+  {
+    "revision": "1eca2ed18a921c9e3b78f283133956eb",
+    "url": "/clicky-game/static/media/old-paper-background.1eca2ed1.jpg"
+  }
+]);
