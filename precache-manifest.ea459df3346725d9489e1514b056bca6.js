4f08371f7aa6d5e6d971395931cf2ba6dd04c3b1
spbryan <seanbryan@comcast.com>
2019-08-10 12:27:55 -0400

    Updates

--- precache-manifest.ea459df3346725d9489e1514b056bca6.js
+++ precache-manifest.ea459df3346725d9489e1514b056bca6.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f3279acb0fa144638d8ef147d98fada5",
+    "revision": "93d7a164698c9dc392d517b0fcc529df",
     "url": "/skyrim-click-game/index.html"
   },
   {
-    "revision": "01639e3b91ca7961bf3a",
-    "url": "/skyrim-click-game/static/css/main.0477c2ae.chunk.css"
+    "revision": "acae58b2dcfc573b5f1a",
+    "url": "/skyrim-click-game/static/css/main.75c74e11.chunk.css"
   },
   {
     "revision": "dd0ed99a380f8d225736",
     "url": "/skyrim-click-game/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "01639e3b91ca7961bf3a",
+    "revision": "acae58b2dcfc573b5f1a",
     "url": "/skyrim-click-game/static/js/main.4e4f9eed.chunk.js"
   },
   {
@@ -22,5 +22,13 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "a614cf48c86b1ed787d5b37490be6ccf",
     "url": "/skyrim-click-game/static/media/background6.a614cf48.jpg"
+  },
+  {
+    "revision": "1774af69ee6a7f11bbd8d2b233026176",
+    "url": "/skyrim-click-game/static/media/header.1774af69.jpg"
+  },
+  {
+    "revision": "1eca2ed18a921c9e3b78f283133956eb",
+    "url": "/skyrim-click-game/static/media/old-paper-background.1eca2ed1.jpg"
   }
 ]);
